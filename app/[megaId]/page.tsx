534c9d7922d6db0e94ca9a60eca827cb17d77c35
etornam45 <navidben45@gmail.com>
2024-03-21 19:13:01 +0000

    Re-structured routes for different layouts

--- app/[megaId]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Routes } from "@/lib/types";
-import { ComponentsMap } from "@/lib/ui.constants";
-
-export default function Page({ params }: { params: { megaId: Routes } }){
-    return <>
-        <h1>{ComponentsMap[params.megaId].component}</h1>
-    </>
-}
