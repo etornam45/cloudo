e9d32b63a09f84d26d05d5a6b758abbbbfac2d30
etornam45 <navidben45@gmail.com>
2024-03-23 09:07:01 +0000

    Added Storage overview

--- pages/overview/OverviewStorage.tsx
+++ pages/overview/OverviewStorage.tsx
@@ -1,47 +1,44 @@
-import { Card } from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
-import { ArchiveIcon, FileTextIcon, ImageIcon, SpeakerLoudIcon, VideoIcon } from "@radix-ui/react-icons";
-import { ReactNode } from "react";
+import { FileTypeI, FileTypes } from "@/lib/prisma";
+import { fileTypesAtom } from "@/lib/store.jotai";
+import { CopyIcon, FileIcon, ImageIcon, ReaderIcon, SpeakerLoudIcon } from "@radix-ui/react-icons";
+import { useAtom } from "jotai";
+import { ReactNode, useEffect } from "react";
+
 
 export default function OverviewStorage() {
+
+    const [fileTypes, setFileTypes] = useAtom<FileTypeI[]>(fileTypesAtom);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/api/filetype');
+            const data = await response.json();
+            setFileTypes(data);
+            console.log(data);
+        }
+        fetchData();
+    }, []);
+
     return (
         <div className="flex gap-4 p-3" style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))" }}>
-            <FileTypesCard fileType="Documents" size_used={10} size_total={20} files={100} color="blue" icon={<FileTextIcon className="w-5 h-5" />} />
-            <FileTypesCard fileType="Images" size_used={19} size_total={20} files={100} color="green" icon={<ImageIcon className="w-5 h-5" />} />
-            <FileTypesCard fileType="Videos" size_used={10} size_total={20} files={100} color="red" icon={<VideoIcon className="w-5 h-5" />} />
-            <FileTypesCard fileType="Audio" size_used={10} size_total={20} files={100} color="yellow" icon={<SpeakerLoudIcon className="w-5 h-5" />} />
-            <FileTypesCard fileType="Others" size_used={10} size_total={20} files={100} color="purple" icon={<ArchiveIcon className="w-5 h-5" />} />
+            {fileTypes.map((fileType) => (
+                <div key={fileType.id} className="flex gap-3 items-center p-1 flex-row bg-gray-100 rounded-md">
+                    <div className="p-4 bg-green-400/20 rounded-sm">{FileTypeIcon[fileType.name]}</div>
+                    <div className="flex flex-col gap-1">
+                        <h2 className="font-semibold">{fileType.name}</h2>
+                        <p>{fileType._count?.files} files</p>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
 
-interface FileTypesCardProps {
-    fileType: string;
-    size_used: number;
-    size_total: number;
-    color: string;
-    files: number;
-    icon: ReactNode;
-}
 
-export const FileTypesCard = (file: FileTypesCardProps) => {
-    return (
-        <Card className="rounded-sm p-3 shadow-none hover:shadow-md" >
-            <div className="flex  flex-col gap-3">
-                <div className="flex items-center gap-3">
-                    <div className="icon w-12 aspect-square flex items-center justify-center bg-slate-500 rounded-sm" style={{ backgroundColor: `color-mix(in srgb, ${file.color} 80%, white 70%)` }}>
-                        {file.icon}
-                    </div>
-                    <div className="info">
-                        <h2 className="text-lg font-bold">{file.fileType}</h2>
-                        <p>{file.files} files</p>
-                    </div>
-                </div>
-                <div className="flex flex-col justify-between w-full">
-                    <Progress value={(file.size_used / file.size_total) * 100} color={file.color} />
-                    <p className="mt-2">{file.size_used} GB {file.size_total} GB</p>
-                </div>
-            </div>
-        </Card>
-    )
-}
+const FileTypeIcon: Record<FileTypes, ReactNode> = {
+    "Documents": <ReaderIcon className="w-6 h-6" />,
+    "Images": <ImageIcon className="w-6 h-6" />,
+    "Audio": <SpeakerLoudIcon className="w-6 h-6" />,
+    "Video": <FileIcon className="w-6 h-6" />,
+    "Others": <CopyIcon className="w-6 h-6" />
+}  
