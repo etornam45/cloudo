3d8ddec59d1f446ec522eee0af34b57fd94d6010
etornam45 <navidben45@gmail.com>
2024-03-23 09:06:26 +0000

    added fileTypes RecentFolder and Folder atom (store)

--- lib/store.jotai.ts
+++ lib/store.jotai.ts
@@ -1,5 +1,16 @@
 import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
-import { UserI } from './prisma';
+import { FileTypeI, FolderI, UserI } from './prisma';
 
 export const logggedInUserAtom = atomWithStorage<UserI | null>("user" ,null);
+export const fileTypesAtom = atom<FileTypeI[]>([]);
+
+export const FoldersAtom = atomWithStorage<FolderI[]>("folders", []);
+
+export const RecentFoldersAtom = atom<FolderI[]>((get) => {
+    const folders = get(FoldersAtom);
+    // Return first 5 folders
+    return folders.slice(0, 5);  
+})
+
+export const openedRightSideBarAtom = atom<boolean>(true)
